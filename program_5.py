8d1ef0260c1c20bc845787b04a8cdd1c1b21bcbf
Violet-Haveman <vkhaveman@students.unwsp.edu>
2025-02-07 20:35:37 -0600

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -4,42 +4,49 @@
 # Also tax is 7%. 
 # Write a program the inputs the type of hot dog wanted and optional toppings.  
 # The program then displays the hot dog cost, tax and total cost.
+
+
 Hot_Dog = 3.5
 Chili_Dog = 4.5
 Cheese = 0.5
 Peppers = 0.75
 Grilled_onions = 1
 Tax = 0.07
-order = str(input("Please state your order: "))
+order = str(input("Please State your full order:"))
 if order == "hot dog":
-    print ("Your total is $", (Hot_Dog*Tax)+Hot_Dog)
+    print("Your total is $", (Hot_Dog * Tax) + Hot_Dog)
 elif order == "hot dog and cheese":
-    print ("Your total is", ((Hot_Dog+Cheese)*Tax)+(Hot_Dog+Cheese))
+    print("Your total is", ((Hot_Dog + Cheese) * Tax) + (Hot_Dog + Cheese))
 elif order == "hot dog and peppers":
-    print ("Your total is", ((Hot_Dog+Peppers)*Tax)+(Hot_Dog+Peppers))
+    print("Your total is", ((Hot_Dog + Peppers) * Tax) + (Hot_Dog + Peppers))
 elif order == "hot dog and grilled onions":
-    print("Your total is", ((Hot_Dog+Grilled_onions)*Tax)+(Hot_Dog+Grilled_onions))
+    print("Your total is", ((Hot_Dog + Grilled_onions) * Tax) + (Hot_Dog + Grilled_onions))
 elif order == "hot dog and cheese and grilled onions":
-    print("Your total is", ((Hot_Dog+Grilled_onions+Cheese)*Tax)+(Hot_Dog+Grilled_onions+Cheese))
+    print("Your total is", ((Hot_Dog + Grilled_onions + Cheese) * Tax) + (Hot_Dog + Grilled_onions + Cheese))
 elif order == "hot dog and grilled onions and peppers":
-    print("Your total is", ((Hot_Dog+Grilled_onions+Peppers)*Tax)+(Hot_Dog+Grilled_onions+Peppers))
+    print("Your total is", ((Hot_Dog + Grilled_onions + Peppers) * Tax) + (Hot_Dog + Grilled_onions + Peppers))
 elif order == "hot dog and grilled onions and cheese and peppers":
-    print("Your total is",((Hot_Dog+Grilled_onions+Cheese+Peppers)*Tax)+(Hot_Dog+Grilled_onions+Peppers+Cheese))
+    print("Your total is",
+          ((Hot_Dog + Grilled_onions + Cheese + Peppers) * Tax) + (Hot_Dog + Grilled_onions + Peppers + Cheese))
 elif order == "hot dog and cheese and peppers":
-    print("Your total is", ((Hot_Dog+Cheese+Peppers)*Tax)+(Hot_Dog+Cheese+Peppers))
+    print("Your total is", ((Hot_Dog + Cheese + Peppers) * Tax) + (Hot_Dog + Cheese + Peppers))
 if order == "chili dog":
-    print ("Your total is $", (Chili_Dog*Tax)+Chili_Dog)
+    print("Your total is $", (Chili_Dog * Tax) + Chili_Dog)
 elif order == "chili dog and cheese":
-    print ("Your total is", ((Chili_Dog+Cheese)*Tax)+(Chili_Dog+Cheese))
+    print("Your total is", ((Chili_Dog + Cheese) * Tax) + (Chili_Dog + Cheese))
 elif order == "chili dog and peppers":
-    print ("Your total is", ((Chili_Dog+Peppers)*Tax)+(Chili_Dog+Peppers))
+    print("Your total is", ((Chili_Dog + Peppers) * Tax) + (Chili_Dog + Peppers))
 elif order == "chili dog and grilled onions":
-    print("Your total is", ((Chili_Dog+Grilled_onions)*Tax)+(Chili_Dog+Grilled_onions))
+    print("Your total is", ((Chili_Dog + Grilled_onions) * Tax) + (Chili_Dog + Grilled_onions))
 elif order == "chili dog and cheese and grilled onions":
-    print("Your total is", ((Chili_Dog+Grilled_onions+Cheese)*Tax)+(Chili_Dog+Grilled_onions+Cheese))
+    print("Your total is", ((Chili_Dog + Grilled_onions + Cheese) * Tax) + (Chili_Dog + Grilled_onions + Cheese))
 elif order == "chili dog and grilled onions and peppers":
-    print("Your total is", ((Chili_Dog+Grilled_onions+Peppers)*Tax)+(Chili_Dog+Grilled_onions+Peppers))
+    print("Your total is", ((Chili_Dog + Grilled_onions + Peppers) * Tax) + (Chili_Dog + Grilled_onions + Peppers))
 elif order == "chili dog and grilled onions and cheese and peppers":
-    print("Your total is",((Chili_Dog+Grilled_onions+Cheese+Peppers)*Tax)+(Chili_Dog+Grilled_onions+Peppers+Cheese))
+    print("Your total is",
+          ((Chili_Dog + Grilled_onions + Cheese + Peppers) * Tax) + (Chili_Dog + Grilled_onions + Peppers + Cheese))
 elif order == "chili dog and cheese and peppers":
-    print("Your total is", ((Chili_Dog+Cheese+Peppers)*Tax)+(Chili_Dog+Cheese+Peppers))
+    print("Your total is", ((Chili_Dog + Cheese + Peppers) * Tax) + (Chili_Dog + Cheese + Peppers))
+
+
+
